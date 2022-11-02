1d895a212c30103db60bfdc965bc8739113c4ffc
yan doro <yan_mvd@hotmail.com>
2022-11-02 18:44:35 -0300

    chore: initial project structure

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import style from './home.module.css'
+
+const Home = () => {
+    return (
+        <div className={style.Wrapper}>
+            <div className={style.TitleWrapper}>
+                <div className={style.Title}>
+                    Home Page
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,16 @@
+import style from './login.module.css'
+
+const Login = () => {
+    return (
+        <div className={style.Wrapper}>
+            <div className={style.TitleWrapper}>
+                <div className={style.Title}>
+                    Login Page
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
+
