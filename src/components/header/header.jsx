597936aaccaa0e5bfa828e828c8fe240fe76cc04
yan doro <yan_mvd@hotmail.com>
2023-02-24 13:23:13 -0300

    feat: adjusts to follow figma design

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,16 @@
-import { FiShoppingCart, FiUser } from 'react-icons/fi'
+import { useRef } from 'react'
+import { FiSearch, FiShoppingCart, FiUser } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import useUser from '../../hooks/useUser'
 import style from './header.module.css'
 
 const Header = () => {
     const { logged } = useUser()
+    const searchInput = useRef(null)
 
+    const hanldeMagnifierClick = () => {
+        searchInput.current.focus()
+    }
     return (
         <header className={style.Wrapper}>
             <div>
@@ -14,7 +19,8 @@ const Header = () => {
                 </Link>
             </div>
             <div className={style.SearchWrapper}>
-                <input type="text" placeholder='search' className={style.SearchBox} />
+                <input type="text" placeholder='search' className={style.SearchBox} ref={searchInput} />
+                <FiSearch size={24} className={style.SearchIcon} onClick={hanldeMagnifierClick} />
             </div>
             <div className={style.ButtonsWrapper}>
                 <Link to={!logged ? "/login" : "/profile"}>
